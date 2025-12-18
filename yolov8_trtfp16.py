072768a18f7c892f0758c8688e358f6b60a30b97
fadi-kidess <102939983+fadi-kidess@users.noreply.github.com>
2025-12-17 23:59:52 -0500

    TRT for YOLO
    These are the source codes programmed to convert YOLOv8 and YOLOv5 (despite the file naming) to TRT for better acceleration for a given quantization format.

---
+++ yolov8_trtfp16.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+# Load a YOLOv8n PyTorch model
+model = YOLO("yolov8n.pt")
+
+# Export the model
+model.export(format="engine",half = True)  # creates 'yolov8n.engine'
+
+# Load the exported TensorRT model
+trt_model = YOLO("yolov8n.engine", task='detect')
+
+# Run inference
+results = trt_model("https://ultralytics.com/images/bus.jpg")
+img = cv2.imread("bus.jpg")
+annotated_img = img.copy()
+print(results)
+
+
+for r in results:
+    boxes = r.boxes
+    for box in boxes:
+        x1, y1, x2, y2 = map(int,box.xyxy[0])
+        cls = int(box.cls[0])
+        conf = float(box.conf[0])
+        class_name = trt_model.names[cls]
+        cv2.rectangle(annotated_img, (x1, y1), (x2, y2), (0,255,0), 2)
+        label = f"{class_name} {conf:.2f}"
+        cv2.putText(annotated_img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+cv2.imwrite("annot_img.jpg",annotated_img)
